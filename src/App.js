8bdc88b21b87658554c11839c5837e66d542f30b
muthu7550 <60920986+muthu7550@users.noreply.github.com>
2022-12-11 08:19:50 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+//  let count=0;
+  const [count, setCount] = useState(1);
+  const increment = () => {
+   setCount(count + 1);
+  };
+  const decrement = () => {
+    if(count > 0)
+      setCount(count - 1);
+  };
+  const reset = () => {
+    setCount(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>react increament app</h1>
+      <h2>count: {count}</h2>
+      < div className='spc'>
+      <div><button  onClick={increment}  className="btn btn-success" >+</button></div>
+      <div><button  onClick={decrement} className="btn btn-danger">-</button></div>
+      <div><button onClick={reset} className="btn btn-warning">reset</button></div>
+
+      </div>
     </div>
   );
 }
